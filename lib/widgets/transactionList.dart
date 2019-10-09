a78ed2fb9be2269af4db3ae80d67de7017c05c4a
EYoung75 <eyoung75.ey@gmail.com>
2019-10-09 15:39:50 -0400

    implemented listview widget to contain list of transactions rather than containing them in a column

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -4,55 +4,58 @@ import 'package:intl/intl.dart';
 import "../models/transaction.dart";
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Card(
-          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-          child: Row(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(5),
-                margin: EdgeInsets.all(10),
-                child: Text(
-                  "\$${transaction.amount}",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.deepOrange),
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.deepOrange,
-                  width: 2,
-                )),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transaction.title,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "\$${transactions[index].amount}",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.deepOrange),
                   ),
-                  Text(
-                    DateFormat.yMMMd().format(transaction.date),
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.deepOrange,
+                    width: 2,
+                  )),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
